1e58ccfb300d5814982705d1ebe9daecd0862a1b
Unknown <unknown@example.com>
2021-01-08 17:42:43 -0500

    Refactor a Root101 + Licence a clases

--- src/main/java/com/root101/module/util/console/ConsoleSwingModule.java
+++ src/main/java/com/root101/module/util/console/ConsoleSwingModule.java
@@ -1,4 +1,20 @@
-package com.jhw.module.util.console;
+/*
+ * Copyright 2021 Root101 (jhernandezb96@gmail.com, +53-5-426-8660).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Or read it directly from LICENCE.txt file at the root of this project.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.root101.module.util.console;
 
 import com.root101.clean.swing.app.AbstractSwingApplication;
 import com.root101.clean.swing.app.DefaultAbstractSwingMainModule;
@@ -9,6 +25,11 @@ import com.root101.swing.material.standards.MaterialIcons;
 import java.awt.event.ActionEvent;
 import javax.swing.AbstractAction;
 
+/**
+ *
+ * @author Root101 (jhernandezb96@gmail.com, +53-5-426-8660)
+ * @author JesusHdezWaterloo@Github
+ */
 public class ConsoleSwingModule extends DefaultAbstractSwingMainModule {
 
     public static final ConsoleFrame consoleFrame = new ConsoleFrame();
@@ -24,7 +45,7 @@ public class ConsoleSwingModule extends DefaultAbstractSwingMainModule {
     @Override
     public void register(AbstractSwingApplication app) {
         registerConsole(app);
-        System.out.println("Bienvenido a la consola..... ☺");
+        System.out.println("Bienvenido/a a la consola..... ☺");
         System.out.println("Aqui se ve todo el trabajo en background del sistema.");
     }
 
